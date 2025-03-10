809574dcb22a7031c74077f23dba065b5b51c2a5
Daniel B <34192225+danielbeach@users.noreply.github.com>
2025-03-09 19:41:27 -0500

    updates from development.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 ## BoggleyWollah
 A Python package to help understand the health of your `LakeHouse`.
+
+
+### What does it do?
+`BoggleyWollah` is made to help you ...
+- Understand the size(s) of the files in your `Lake House` ... are they too big or small?
+- Understand if you have "dead files" (unreferenced files).
+- Data skew via inspecting your partitioning or clustering strategy.
+
+### Lake House Support
+The goal is to support both `Delta Lake` and `Iceberg` tables in `s3`.
+
+### Usage
+```
+logging.basicConfig(level=logging.INFO)
+boggley = BoggleyWollah("delta", "confessions-of-a-data-guy", ["some/table/some/location/"])
+boggley.analyze_tables()
+
+INFO:botocore.credentials:Found credentials in environment variables.
+INFO:root:Analyzing table: some/table/some/location
+INFO:root:Table metrics: {'total_size_mb': 1238.24, 'total_parquet_files': 176, 'average_file_size_mb': 7.04}
+INFO:root:For table some/table/some/location Average file size 7.04 MB is less than 256 MB, you should run a compaction job
+INFO:root:Dead files found 175: 
+ ```
--- src/boggley.py
+++ src/boggley.py
@@ -1,5 +1,7 @@
 import logging
 import boto3
+from deltalake import DeltaTable
+
 
 class BoggleyWollah:
     def __init__(self, 
@@ -16,6 +18,7 @@ class BoggleyWollah:
         self.s3_client = boto3.client("s3")
         self.table_files = {}
         self.table_metrics = {}
+
         self.init_checks()
 
     def init_checks(self) -> str:
@@ -35,22 +38,44 @@ class BoggleyWollah:
             for obj in page["Contents"]:
                 key = obj["Key"]
                 size = obj["Size"] / (1024 * 1024)
-                files.append({"Key": key, "size": size})
+                last_modified = obj["LastModified"]
+                files.append({"Key": key, "size": size, "modified": last_modified})
         return files
-    
+        
     def calculate_table_metrics(self, table, table_files: list) -> dict:
         ## Tables smaller than 2.56 TB, the autotuned target file size is 256 MB
         total_size = 0
         total_files = 0
+        parquet_files = []
         for file in table_files:
             if '.parquet' in file["Key"]:
                 total_size += file["size"]
                 totl_size_rounded = round(total_size, 2)
                 total_files += 1
+                parquet_files.append(self.strip_down_parquet_location(file["Key"]))
 
         average_file_size = round(totl_size_rounded/total_files,2)
         self.table_metrics = {table : {"total_size_mb": totl_size_rounded, "total_parquet_files": total_files, "average_file_size_mb": average_file_size}}
         logging.info(f"Table metrics: {self.table_metrics[table]}")
+        return parquet_files
+
+    def strip_down_parquet_location(self, file):
+        last_slash_index = file.rfind("/")
+        result = file[last_slash_index + 1:]
+        return result
+
+    def find_dead_files(self, actual_files, valid_files):
+        actual_files_only = []
+        for file in actual_files:
+            if 'chcekpoint' not in file:
+                actual_files_only.append(self.strip_down_parquet_location(file))
+        dead_files = list(set(actual_files_only) - set(valid_files))
+        if dead_files:
+            cnt = len(dead_files)
+            logging.info(f"Dead files found {cnt} ")
+        else:
+            logging.info("No dead files found")
+        return dead_files
 
 
     def check_average_file_size(self, table) -> None:
@@ -63,11 +88,13 @@ class BoggleyWollah:
     def analyze_tables(self):
         for table in self._s3_keys:
             logging.info(f"Analyzing table: {table}")
+            table_info = DeltaTable(f"s3://{self._s3_bucket}/{table}")
+            valid_files = table_info.files()
             table_files = self.pull_all_s3_files(table)
             self.table_files[table] = table_files
-            self.calculate_table_metrics(table, table_files)
+            parquet_files = self.calculate_table_metrics(table, table_files)
             self.check_average_file_size(table)
-            
+            dead_files = self.find_dead_files(parquet_files, valid_files)
 
 
 if __name__ == "__main__":
